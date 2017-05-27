83dd3cd4bfa0feb712f68aa6fb5abae3b721ed3a
lar3811 <lar3811@yandex.ru>
2017-05-27 22:26:25 +0500

    Added queue adapters

---
+++ Echo/src/QueueAdapters/QueueAdapter.cs
@@ -0,0 +1,33 @@
+﻿using Echo.Abstract;
+using System.Collections.Generic;
+
+namespace Echo.QueueAdapters
+{
+    public class QueueAdapter : IEchoQueue
+    {
+        private readonly Queue<Echo> _queue;
+
+        public QueueAdapter() : this(new Queue<Echo>()) { }
+        public QueueAdapter(Queue<Echo> queue)
+        {
+            _queue = queue;
+        }
+
+        public int Count => _queue.Count;
+
+        public void Clear()
+        {
+            _queue.Clear();
+        }
+
+        public Echo Dequeue()
+        {
+            return _queue.Dequeue();
+        }
+
+        public void Enqueue(Echo echo)
+        {
+            _queue.Enqueue(echo);
+        }
+    }
+}
---
+++ Echo/src/QueueAdapters/StackAdapter.cs
@@ -0,0 +1,33 @@
+﻿using Echo.Abstract;
+using System.Collections.Generic;
+
+namespace Echo.QueueAdapters
+{
+    public class StackAdapter : IEchoQueue
+    {
+        private readonly Stack<Echo> _stack;
+
+        public StackAdapter() : this(new Stack<Echo>()) { }
+        public StackAdapter(Stack<Echo> stack)
+        {
+            _stack = stack;
+        }
+
+        public int Count => _stack.Count;
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+
+        public Echo Dequeue()
+        {
+            return _stack.Pop();
+        }
+
+        public void Enqueue(Echo echo)
+        {
+            _stack.Push(echo);
+        }
+    }
+}
