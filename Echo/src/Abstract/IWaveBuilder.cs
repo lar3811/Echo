3cdec9c4d21fa3eaaca8427f0c3121579b98b226
lar3811 <lar3811@yandex.ru>
2017-06-22 20:10:23 +0500

    Added IWaveBuilder and ICloneable<T> interfaces

---
+++ Echo/src/Abstract/IWaveBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo.Abstract
+{
+    public interface IWaveBuilder<TWave> where TWave : IWave
+    {
+        TWave Create(Vector3 location, Vector3 direction);
+        //TODO: TWave Create(TWave wave, Vector3 location, Vector3 direction); -- OR -- TWave Create(TWave wave, Vector3 offset, Vector3 direction);
+        TWave Create(TWave wave, Vector3 direction);
+    }
+}
---
+++ Echo/src/Waves/WaveBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Echo.Abstract;
+using Echo.Waves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Echo.Waves
+{
+    public class WaveBuilder : IWaveBuilder<Wave>
+    {
+        public Wave Create(Vector3 location, Vector3 direction)
+        {
+            return new Wave(location, direction);
+        }
+
+        public Wave Create(Wave wave, Vector3 direction)
+        {
+            return new Wave(wave, direction);
+        }
+
+
+
+        public class Generic<TData> : IWaveBuilder<Wave.Generic<TData>> where TData : ICloneable<TData>
+        {
+            public Wave.Generic<TData> Create(Vector3 location, Vector3 direction)
+            {
+                return new Wave.Generic<TData>(location, direction);
+            }
+
+            public Wave.Generic<TData> Create(Wave.Generic<TData> wave, Vector3 direction)
+            {
+                return new Wave.Generic<TData>(wave, direction);
+            }
+        }
+    }
+}
